7ec3db038b9d007c75585559c23f5ff5777c7c2a
Anna <owner@10-17-92-121.dynapool.wireless.nyu.edu>
2018-12-20 12:37:51 -0500

    Last commit

--- final_part_A_aen315.py
+++ final_part_A_aen315.py
@@ -6,7 +6,24 @@ import pandas as pd
 import sys
 
 from pandas import Series, DataFrame
-import scipy.cluster.hierarchy as hc
+import scipy.cluster.hierarchy as hc 
 
 exp = pd.read_table('tetrahymena.tsv', delim_whitespace=True)
-print(exp)
+#loaded file into a dataframe with the given names as column names.
+x = exp['diameter']>= 19.2
+#in the read table x is the diameter if its greater than or equal to 19.2
+exp = exp[x] 
+y = exp['diameter']<= 26.0
+#in the read table y is the diameter if its less than or equal to 26
+exp = exp[y]
+
+exp['log_concentration'] = np.log(exp.conc)
+exp['log_diameter'] = np.log(exp.diameter)
+
+plot1 = sns.pairplot(x_vars=['conc'], y_vars=['diameter'], data=exp, hue= 'glucose',markers= ['x','o'], height=5)
+plot2 = sns.pairplot(x_vars=['log_concentration'], y_vars=['log_diameter'], data=exp, hue= 'glucose',markers= ['x','o'],height=5)
+#markers changes shape of markers for glucose depending on its presence
+
+plot1.savefig("final_part_A_nonlog_aen315.pdf")
+plot2.savefig("final_part_A_log_aen315.pdf")
+
